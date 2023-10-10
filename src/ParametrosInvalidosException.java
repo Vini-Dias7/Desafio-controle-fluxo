208c4aca23b1af62764668a4f7b48359b425a49d
Vinicius <viniciusnot106@gmail.com>
2023-10-10 11:36:23 -0300

    desafio de controle de fluxo

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public static void main(String[] args) {
+        System.out.println("O segundo parametro tem que ser menor que o primeiro!");
+    }
+}
